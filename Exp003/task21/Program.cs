ec8906f70a9a5a3ab7c6a443701cbdfa793a7d65
MaxiRage <iarostiuk@ya.ru>
2022-09-07 21:54:25 +0300

    exp3 done

---
+++ Exp003/task21/Program.cs
@@ -0,0 +1,19 @@
+﻿
+/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.WriteLine("Введите координаты первой точки в формате X,X,X ");
+string coord = Console.ReadLine();
+Console.WriteLine("Введите координаты второй точки в формате X,X,X ");
+coord +="," + Console.ReadLine();
+Console.WriteLine("-------------");
+
+string [] str = coord.Split(",");
+int [] points = Array.ConvertAll(str, int.Parse);
+
+double disatnce = Math.Sqrt(Math.Pow(points[3] - points[0], 2) + Math.Pow(points[4] - points[1], 2) + Math.Pow(points[5] - points[2], 2));
+
+Console.WriteLine("Расстояние между точками = " + Math.Round(disatnce, 2));
---
+++ Exp003/task23/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("-------------");
+int cube = 1;
+
+while (cube <= numberA)
+{
+    Console.Write(cube * cube * cube + " ");
+    cube++;
+
+}
