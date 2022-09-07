5212dd8907f0801e36496296129818a8022aab50
MaxiRage <iarostiuk@ya.ru>
2022-09-07 21:56:22 +0300

    exp3 done

--- Exp003/task21/Program.cs
+++ Exp003/task21/Program.cs
@@ -8,11 +8,11 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 Console.WriteLine("Введите координаты первой точки в формате X,X,X ");
 string coord = Console.ReadLine();
 Console.WriteLine("Введите координаты второй точки в формате X,X,X ");
-coord +="," + Console.ReadLine();
+coord += "," + Console.ReadLine();
 Console.WriteLine("-------------");
 
-string [] str = coord.Split(",");
-int [] points = Array.ConvertAll(str, int.Parse);
+string[] str = coord.Split(",");
+int[] points = Array.ConvertAll(str, int.Parse);
 
 double disatnce = Math.Sqrt(Math.Pow(points[3] - points[0], 2) + Math.Pow(points[4] - points[1], 2) + Math.Pow(points[5] - points[2], 2));
 
