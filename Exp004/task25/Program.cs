cb4977a2e78d15b77fb0d546f834c2c7d0e9fa92
MaxiRage <iarostiuk@ya.ru>
2022-09-10 17:24:42 +0300

    exp4 done

---
+++ Exp004/task25/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+Console.WriteLine("Введите 2 числа в формате X, X ");
+string nums = Console.ReadLine();
+Console.WriteLine(Degree(nums));
+
+int Degree(string nums)
+{
+    string[] tmp = nums.Split(", ");
+    int[] digitArr = Array.ConvertAll(tmp, int.Parse);
+    int res = digitArr[0];
+    for (int i = 1; i < digitArr[1]; i++)
+    {
+        res = res * digitArr[0];
+    }
+    return res;
+}
---
+++ Exp004/task29/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.WriteLine("Введите числа в формате X, X, ... X ");
+string nums = Console.ReadLine();
+Console.WriteLine("[{0}]", string.Join(", ", Array(nums)));
+
+string[] Array(string nums)
+{
+    string[] tmp = nums.Split(", ");
+    return tmp;
+}
